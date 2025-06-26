5ff14908b2f37b6e1f375fcd71d7d17f19b51b33
Joshua Garde <jgarde@jpl.nasa.gov>
2025-06-26 15:18:51 -0700

    Fix Docker image pushing

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,17 +1,18 @@
-resource "aws_ecr_repository" "confluence" {
-  for_each = toset(var.docker_images)
-  name = each.value
+data "aws_ecr_authorization_token" "default" {}
 
-  image_scanning_configuration {
-    scan_on_push = true
+locals {
+  images = {
+    for image in var.docker_images : image => {
+      source_name = "${var.docker_registry}/${image}:${var.app_version}"
+      destination_repository = "${var.prefix}-${regex(".+\\/(.+)",image)[0]}"
+      destination_name = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/${var.prefix}-${regex(".+\\/(.+)",image)[0]}:${var.app_version}"
+    }
   }
 }
 
-data "aws_ecr_authorization_token" "default" {}
-
 data "docker_registry_image" "confluence" {
-  for_each = aws_ecr_repository.confluence
-  name = "${var.docker_registry}/${each.value.name}:${var.app_version}"
+  for_each = local.images
+  name = each.value.source_name
 }
 
 resource "docker_image" "confluence" {
@@ -20,19 +21,33 @@ resource "docker_image" "confluence" {
   pull_triggers = [each.value.sha256_digest]
 }
 
+resource "aws_ecr_repository" "confluence" {
+  for_each = local.images
+  name = each.value.destination_repository
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
 resource "docker_tag" "confluence" {
-  for_each = docker_image.confluence
-  source_image = each.value.name
-  target_image = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/${regex(".+\\/(.+\\/.+)", each.value.name)[0]}"
+  depends_on = [ docker_image.confluence ]
+
+  for_each = local.images
+  source_image = "${each.value.source_name}"
+  target_image = "${each.value.destination_name}"
 }
 
 resource "docker_registry_image" "confluence" {
+  depends_on = [ aws_ecr_repository.confluence ]
   for_each = docker_tag.confluence
+
+  name = each.value.target_image
+  keep_remotely = true
+
   lifecycle {
     ignore_changes = [auth_config]
   }
-  name = each.value.target_image
-  keep_remotely = true
 
   auth_config {
     address = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com"
