495a99fb1dca5e272ddd7516453054c362dc8943
Joshua Garde <jgarde@jpl.nasa.gov>
2025-06-30 10:54:26 -0700

    Refactor docker image push mechanism

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -10,17 +10,6 @@ locals {
   }
 }
 
-data "docker_registry_image" "confluence" {
-  for_each = local.images
-  name = each.value.source_name
-}
-
-resource "docker_image" "confluence" {
-  for_each = data.docker_registry_image.confluence
-  name = each.value.name
-  pull_triggers = [each.value.sha256_digest]
-}
-
 resource "aws_ecr_repository" "confluence" {
   for_each = local.images
   name = each.value.destination_repository
@@ -30,28 +19,23 @@ resource "aws_ecr_repository" "confluence" {
   }
 }
 
-resource "docker_tag" "confluence" {
-  depends_on = [ docker_image.confluence ]
-
+data "docker_registry_image" "confluence" {
   for_each = local.images
-  source_image = "${each.value.source_name}"
-  target_image = "${each.value.destination_name}"
+  name = each.value.source_name
 }
 
-resource "docker_registry_image" "confluence" {
-  depends_on = [ aws_ecr_repository.confluence ]
-  for_each = docker_tag.confluence
-
-  name = each.value.target_image
-  keep_remotely = true
+resource "terraform_data" "docker_registry_image" {
+  for_each = data.docker_registry_image.confluence
 
   lifecycle {
-    ignore_changes = [auth_config]
+    replace_triggered_by = [ each.value.sha256_digest ]
   }
 
-  auth_config {
-    address = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com"
-    username = data.aws_ecr_authorization_token.default.user_name
-    password = data.aws_ecr_authorization_token.default.password
-  }
+  provisioner "local-exec" {
+    command = (
+      "docker pull ${each.value.name} && " +
+      "docker tag ${each.value.name} ${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/${var.prefix}-${regex(".+\\/(.+)",image)[0]}:${var.app_version} && " +
+      "docker push ${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.prefix}-${regex(".+\\/(.+)",image)[0]}:${var.app_version} && " +
+      "docker rmi ${each.value.name} ${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.prefix}-${regex(".+\\/(.+)",image)[0]}:${var.app_version}"
+    )
 }
